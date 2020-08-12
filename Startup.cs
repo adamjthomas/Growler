e76db479ba694ffc3bed09859f6e974a7d3aecfe
Maddie Thomas <7356701+madelynthomas@users.noreply.github.com>
2020-08-12 14:53:48 -0700

    Added ConnectionString to our SQL Server

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 using Growler.Repo;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -9,6 +10,8 @@ namespace Growler
 {
     public class Startup
     {
+        private string _connection = null;
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -18,6 +21,8 @@ namespace Growler
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<GrowlerContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("GrowlerConnection")));
+
             services.AddControllers().AddJsonOptions(opt => opt.JsonSerializerOptions.PropertyNamingPolicy = null);
 
             services.AddScoped<IGrowlerRepo, MockGrowlerRepo>();
